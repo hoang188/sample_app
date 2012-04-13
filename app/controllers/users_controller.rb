792901a576af0649b1640c29e1d7a2eb2840b1cc
hoang188 <hoang188@gmail.com>
2012-04-13 23:57:32 +0700

    init following users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,8 +52,7 @@ class UsersController < ApplicationController
   
   
   private
-    
-    
+
     def correct_user
       @user = User.find(params[:id])
       redirect_to root_path unless current_user?(@user)
