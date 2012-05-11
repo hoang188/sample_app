cbd2e523aa701aef8583f221425570396399b0e7
hoang188 <hoang188@gmail.com>
2012-05-11 16:43:22 +0700

    a

--- config/application.rb
+++ config/application.rb
@@ -61,7 +61,7 @@ module SampleApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
     # CKEditor
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
