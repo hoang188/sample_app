9a62e99e7428ea04e56b5258bd46320d2d28d71d
hoang188 <hoang188@gmail.com>
2012-05-11 16:22:44 +0700

    remove ckeditor::

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,7 +1,7 @@
-class Ckeditor::Picture < Ckeditor::Asset
+class Picture < Ckeditor::Asset
   has_attached_file :data,
                     :url  => "/assets/pictures/:id/:style_:basename.:extension",
-                    :path => ":rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension",
+                    :path => ":rails_root/public/assets/pictures/:id/:style_:basename.:extension",
 	                  :styles => { :content => '800>', :thumb => '118x100#' }
 	
 	validates_attachment_size :data, :less_than => 2.megabytes
